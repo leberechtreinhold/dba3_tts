3b60a05b5d7c3159bc294cbdb763483a3143357b
Marc Pawlowsky <marcpawl@gmail.com>
2021-01-03 20:16:45 -0500

    Define utility bounding_box to get the n-dimensional bounding box from a shape defined by corners.

--- unittests/test_utilities.lua
+++ unittests/test_utilities.lua
@@ -15,4 +15,30 @@ function test_vec_dot_product_2d()
   lu.assertEquals(actual, 22)
 end
 
+-- Returns the minimum and maximum value of each ordinate.
+-- n-dimensional bounding box
+function test_bounding_box()
+  local p1 = {x=12,z=23}
+  local p2 = {x=9,z=48}
+  local p3 = {x=48,z=93}
+  local p4 = {x=56,z=63}
+  local shape = {p1, p2, p3, p4}
+  local actual = bounding_box(shape)
+ 
+  local expected={min={x=9,z=23}, max={x=56,z=93}}
+  lu.assertEquals(actual, expected)
+end
+
+function test_bounding_box_with_named_corners()
+  local p1 = {x=12,z=23}
+  local p2 = {x=9,z=48}
+  local p3 = {x=48,z=93}
+  local p4 = {x=56,z=63}
+  local shape = {tl=p1, tr=p2, bl=p3, br=p4}
+  local actual = bounding_box(shape)
+ 
+  local expected={min={x=9,z=23}, max={x=56,z=93}}
+  lu.assertEquals(actual, expected)
+end
+
 os.exit( lu.LuaUnit.run() )
