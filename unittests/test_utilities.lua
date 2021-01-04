c6ae0f7c9324cf00d3352fcbbb4be999ada14b53
Marc Pawlowsky <marcpawl@gmail.com>
2021-01-03 21:06:51 -0500

    Add utility functions: is_point_in_bounding_box, is_bounding_boxes_intersecting

--- unittests/test_utilities.lua
+++ unittests/test_utilities.lua
@@ -24,7 +24,7 @@ function test_bounding_box()
   local p4 = {x=56,z=63}
   local shape = {p1, p2, p3, p4}
   local actual = bounding_box(shape)
- 
+
   local expected={min={x=9,z=23}, max={x=56,z=93}}
   lu.assertEquals(actual, expected)
 end
@@ -36,27 +36,51 @@ function test_bounding_box_with_named_corners()
   local p4 = {x=56,z=63}
   local shape = {tl=p1, tr=p2, bl=p3, br=p4}
   local actual = bounding_box(shape)
- 
+
   local expected={min={x=9,z=23}, max={x=56,z=93}}
   lu.assertEquals(actual, expected)
 end
 
-function test_point_in_bounding_box()
+function test_point_is_in_bounding_box()
   local box={min={x=9,z=23}, max={x=56,z=93}}
   local point={x=33,z=78}
   lu.assertTrue(is_point_in_bounding_box(point,box))
 end
 
-function test_point_to_right_bounding_box()
+function test_point_is_to_right_of_bounding_box()
   local box={min={x=9,z=23}, max={x=56,z=93}}
   local point={x=3,z=78}
   lu.assertFalse(is_point_in_bounding_box(point,box))
 end
 
-function test_point_to_above_bounding_box()
+function test_point_is_above_bounding_box()
   local box={min={x=9,z=23}, max={x=56,z=93}}
   local point={x=33,z=178}
   lu.assertFalse(is_point_in_bounding_box(point,box))
+end
+
+function test_is_in_range_too_small()
+  lu.assertFalse( is_in_range(12, 9, 23))
+end
+
+function test_is_in_range_too_large()
+  lu.assertFalse( is_in_range(12, 99, 23))
+end
+
+function test_is_in_range()
+  lu.assertTrue( is_in_range(12, 19, 23))
+end
+
+function test_is_bounding_boxes_intersecting_return_true()
+  local box1={min={x=9,z=23}, max={x=56,z=93}}
+  local box2={min={x=19,z=27}, max={x=66,z=103}}
+  lu.assertTrue( is_bounding_boxes_intersecting(box1, box2))
+end
+
+function test_is_bounding_boxes_intersecting_return_false()
+  local box1={min={x=9,z=23}, max={x=56,z=93}}
+  local box2={min={x=87,z=1}, max={x=107,z=103}}
+  lu.assertFalse( is_bounding_boxes_intersecting(box1, box2))
 end
 
 
