660ee608ec62cd0a5c2789c2b54d393cdbfaf873
Marc Pawlowsky <marcpawl@gmail.com>
2021-02-12 08:55:00 -0500

    Test case for army base names being as described on page 7

---
+++ unittests/test_armies.lua
@@ -0,0 +1,129 @@
+lu = require('externals/luaunit/luaunit')
+function dofile (filename)
+  local f = assert(loadfile(filename))
+  return f()
+
+end
+armies={}
+dofile("../scripts/data/data_armies_book_I.ttslua")
+dofile("../scripts/data/data_armies_book_II.ttslua")
+dofile("../scripts/data/data_armies_book_III.ttslua")
+dofile("../scripts/data/data_armies_book_IV.ttslua")
+
+local function starts_with(str, start)
+   return str:sub(1, #start) == start
+end
+
+local function ends_with(str, ending)
+   return ending == "" or str:sub(-#ending) == ending
+end
+
+
+function remove_suffix(s, suffix)
+    return s:gsub("%" .. suffix, "")
+end
+
+function normalize_base_name(name)
+  name = remove_suffix(name, "_Gen")
+  return name
+end
+
+function assert_base_name_valid(name)
+  local valid= {}
+  -- Elephants
+  valid['El']=true
+  -- Knights
+  valid['3Kn']=true 
+  valid['4Kn']=true 
+  valid['6Kn']=true 
+  valid['HCh']=true
+  -- Cavalry
+  valid['Cv']=true 
+  valid['3Cv']=true 
+  valid['6Cv']=true 
+  valid['LCh']=true
+  -- Light Horse
+  valid['2LH']=true
+  valid['LH']=true 
+  valid['LCm']=true 
+  valid['2Cm']=true
+  -- Scythed Chariots
+  valid['SCh']=true
+  -- Camelry
+  valid['Cm']=true
+  -- Spears
+  valid['Sp']=true 
+  valid['8Sp']=true
+  -- Pikes
+  valid['4Pk']=true 
+  valid['3Pk']=true
+  -- Blades
+  valid['4Bd']=true 
+  valid['3Bd']=true 
+  valid['6Bd']=true
+  -- Auxilia
+  valid['4Ax']=true
+  valid['3Ax']=true
+  -- Bows
+  valid['4Bw']=true
+  valid['3Bw']=true 
+  valid['8Bw']=true
+  valid['4Cb']=true 
+  valid['3Cb']=true 
+  valid['8Cb']=true
+  valid['4Lb']=true 
+  valid['3Lb']=true 
+  valid['8Lb']=true
+  valid['3Bw_Mtd']=true 
+  valid['3Bw-Mtd']=true 
+  valid['3Mtd-Bw']=true 
+  valid['3Bw:Mtd']=true
+  valid['4Bw-Mtd']=true 
+  valid['Mtd-4Bw']=true 
+  valid['4Bw_Mtd']=true
+  valid['4Cb_Mtd']=true
+  valid['4Lb_Mtd']=true 
+  valid['4Lb-Mtd']=true
+  -- Psiloi
+  valid['Ps']=true
+  -- Warbands
+  valid['4Wb']=true
+  valid['3Wb']=true
+  -- Hordes
+  valid['7Hd']=true 
+  valid['5Hd']=true
+  -- Artillery
+  valid['Art']=true
+  -- War Wagons
+  valid['WWg']=true
+  -- Ceneral
+  valid['Cp']=true 
+  valid['CP']=true 
+  valid['3CP']=true 
+  valid['Lit']=true 
+  valid['CWg']=true
+  valid['Camp']=true
+
+  local n_name = normalize_base_name(name)
+  if valid[n_name] then
+    return
+  end
+  print("Invalid name ", name, " ", n_name)
+end
+
+-- page 7 lists the abbreviations for the bases.  Verify only the correct
+-- abbreviations are used in the army lists.
+function test_bases_are_in_list()
+  for book_name, book in pairs(armies) do
+    for army_name, army in pairs(armies[book_name]) do
+      for k,v in pairs(army) do
+        if starts_with(k, "base") then
+          local name = v.name
+          assert_base_name_valid(name)
+        end
+      end
+    end
+  end
+end
+
+os.exit( lu.LuaUnit.run() )
